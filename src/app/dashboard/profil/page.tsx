6af8e40c209b516453919f862c188c8bea8feea6
smkntinut2026-lgtm <smkntinut2026@gmail.com>
2026-05-28 08:37:28 +0800

    Update page.tsx

--- src/app/dashboard/profil/page.tsx
+++ src/app/dashboard/profil/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, useRef } from 'react'
 import { createClient } from '@/lib/supabase/client'
-import { useApp } from '../layout'
+import { useApp } from '../context'
 import { Save, Upload, School, X, Image as ImageIcon } from 'lucide-react'
 import toast from 'react-hot-toast'
 import Image from 'next/image'
